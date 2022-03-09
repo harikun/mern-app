258f15e77877868f87821b61f0a45bfebce93dea
Cipto Suhari <cipto.suheri@gmail.com>
2022-03-09 21:55:52 +0700

    create avatar.jsx and css then export to UserItem

--- FRONTEND/src/user/components/UserItem.jsx
+++ FRONTEND/src/user/components/UserItem.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Avatar from "../../shared/components/UIElements/Avatar";
 
 import "./UserItem.css";
 
@@ -7,7 +8,7 @@ const UserItem = (props) => {
     <li className="user-item">
       <div className="user-item__content">
         <div className="user-item__image">
-          <img src={props.image} alt={props.name} />
+          <Avatar image={props.image} alt={props.name} />
         </div>
 
         <div className="user-item__info">
