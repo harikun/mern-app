3d8061cd691cc8d8e0dfbe6bdabb0cca7501dd1d
Cipto Suhari <cipto.suheri@gmail.com>
2022-03-09 21:33:56 +0700

    add jsx code to render html component in useritem.jsx

--- FRONTEND/src/user/pages/Users.jsx
+++ FRONTEND/src/user/pages/Users.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <UsersList />;
+  const USERS = [
+    {
+      id: "u1",
+      image:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      name: "Maximilian",
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
