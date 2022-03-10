310a4f44ed6b0447ca2a5bae6df77e8274ad7239
Cipto Suhari <cipto.suheri@gmail.com>
2022-03-10 20:07:41 +0700

    add styling on mainheader and mainnavigation the import mainnavigation to app.js

--- FRONTEND/src/App.js
+++ FRONTEND/src/App.js
@@ -6,20 +6,24 @@ import {
   Switch,
 } from "react-router-dom";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import Users from "./user/pages/Users";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/">
-          <Users />
-        </Route>
-        <Route exact path="/places/new">
-          <NewPlace />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route exact path="/">
+            <Users />
+          </Route>
+          <Route exact path="/places/new">
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   );
 }
