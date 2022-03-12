b3c86787ca6e2d2f1478483b5c2b0dbcc2d0c52b
Cipto Suhari <cipto.suheri@gmail.com>
2022-03-13 06:34:25 +0700

    add maps but from udeydiscussion trik not from course scesific

---
+++ FRONTEND/src/shared/components/UIElements/Map.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./Map.css";
+
+const Map = (props) => {
+  return (
+    <div className={`map ${props.className}`} style={`props.style`}>
+      This is maps for google but unfortunately it is not working.+ because use
+      google maps api need creditcard and i dont have one. so i use dummy one
+      using iframe from udemyforum
+    </div>
+  );
+};
+
+export default Map;
