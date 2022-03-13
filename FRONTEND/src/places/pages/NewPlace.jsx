85858cd4ff5650c1727bcf230d9433c686f244e8
Cipto Suhari <cipto.suheri@gmail.com>
2022-03-13 19:02:39 +0700

    managing state input component

--- FRONTEND/src/places/pages/NewPlace.jsx
+++ FRONTEND/src/places/pages/NewPlace.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Input from "../../shared/components/FormElements/Input";
+
+import "./NewPlace.css";
 
 const NewPlace = () => {
-  return <form className="place-form"></form>;
+  return (
+    <form className="place-form">
+      <Input
+        element="input"
+        type="text"
+        label="title"
+        validators={[]}
+        errorText="Please enter a valid title."
+      />
+    </form>
+  );
 };
 
 export default NewPlace;
