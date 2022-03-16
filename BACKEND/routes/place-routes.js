4018eb1905136dbce6ad3d96e6dae55abc9aa938
Cipto Suhari <cipto.suheri@gmail.com>
2022-03-16 19:48:37 +0700

    Handling Errors

--- BACKEND/routes/place-routes.js
+++ BACKEND/routes/place-routes.js
@@ -34,13 +34,25 @@ const DUMMY_PLACES = [
 router.get("/:pid", (req, res, next) => {
   const placeId = req.params.pid;
   const place = DUMMY_PLACES.find((p) => p.id === placeId);
+
+  if (!place) {
+    const error = new Error("Could not find place for provided id!");
+    error.code = 404;
+    throw error;
+  }
+
   res.json({ place });
 });
 
 router.get("/user/:uid", (req, res, next) => {
   const userId = req.params.uid;
-  const places = DUMMY_PLACES.filter((p) => p.creator === userId);
-  res.json({ places });
+  const place = DUMMY_PLACES.filter((p) => p.creator === userId);
+  if (!place) {
+    const error = new Error("Could not find place for provided user  id!");
+    error.code = 404;
+    return next(error);
+  }
+  res.json({ place });
 });
 
 export default router;
