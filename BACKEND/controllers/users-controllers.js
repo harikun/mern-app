9b44238a3713e1c4b8fc658b690cadc777bbd9e4
Cipto Suhari <cipto.suheri@gmail.com>
2022-03-17 20:38:12 +0700

    Setting Up the User Routes (Signup, Login, Get Users)

---
+++ BACKEND/controllers/users-controllers.js
@@ -0,0 +1,47 @@
+const HttpError = require("../models/http-error");
+const { v4: uuidv4 } = require("uuid");
+
+const DUMMY_USERS = [
+  {
+    id: "u1",
+    name: "Max",
+    email: "test@tes.com",
+    password: "test",
+  },
+];
+
+const getUsers = (req, res, next) => {
+  res.json({ users: DUMMY_USERS });
+};
+
+const signup = (req, res, next) => {
+  const { name, email, password } = req.body;
+  const existingUser = DUMMY_USERS.find((u) => u.email === email);
+  if (existingUser) {
+    throw new HttpError("Could not create user. Email already exists.", 422);
+  }
+  const user = {
+    id: uuidv4(),
+    name,
+    email,
+    password,
+  };
+  DUMMY_USERS.push(user);
+  res.status(201).json({ user });
+};
+
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+  const user = DUMMY_USERS.find((u) => u.email === email);
+  if (!user || user.password !== password) {
+    throw new HttpError(
+      "Could not find user with the provided credentials.",
+      401
+    );
+  }
+  res.json({ user });
+};
+
+exports.getUsers = getUsers;
+exports.signup = signup;
+exports.login = login;
