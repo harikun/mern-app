51886028bf0aca0c5d6f399d3cbd6fffb4c178b5
Cipto Suhari <cipto.suheri@gmail.com>
2022-03-23 18:54:24 +0700

    delete updating place dummy data

--- BACKEND/controllers/places-controllers.js
+++ BACKEND/controllers/places-controllers.js
@@ -118,12 +118,11 @@ const updatePlace = (req, res, next) => {
   const { title, description } = req.body;
   const placeId = req.params.pid;
 
-  const updatedPlace = { ...DUMMY_PLACES.find((p) => p.id === placeId) };
-  const placeIndex = DUMMY_PLACES.findIndex((p) => p.id === placeId);
+
   updatedPlace.title = title;
   updatedPlace.description = description;
 
-  DUMMY_PLACES[placeIndex] = updatedPlace;
+
 
   res.status(200).json({ place: updatedPlace });
 };
