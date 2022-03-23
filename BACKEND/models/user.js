ffed61f40e4134357d13d646d3fd05251b855f73
Cipto Suhari <cipto.suheri@gmail.com>
2022-03-23 20:52:27 +0700

    import mongoose uniqu validator dan export user schemaa

--- BACKEND/models/user.js
+++ BACKEND/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const Schema = mongoose.Schema;
 
@@ -26,3 +27,7 @@ const userSchema = new Schema({
     required: true,
   },
 });
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("User", userSchema);
