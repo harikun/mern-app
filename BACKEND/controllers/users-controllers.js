df39c65be123357df26fea4a9acadeada8b07426
Cipto Suhari <cipto.suheri@gmail.com>
2022-03-24 22:09:09 +0700

    import user model

--- BACKEND/controllers/users-controllers.js
+++ BACKEND/controllers/users-controllers.js
@@ -1,6 +1,7 @@
 const HttpError = require("../models/http-error");
 const { v4: uuidv4 } = require("uuid");
 const { validationResult } = require("express-validator");
+const User = require("../models/user");
 
 const DUMMY_USERS = [
   {
