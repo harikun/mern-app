2ae51bcca54c97dd327c987c6977331aef7b0b39
Cipto Suhari <cipto.suheri@gmail.com>
2022-03-28 20:31:36 +0700

    get rid of dummy user

--- FRONTEND/src/user/pages/Users.jsx
+++ FRONTEND/src/user/pages/Users.jsx
@@ -2,15 +2,6 @@ import React from "react";
 import UsersList from "../components/UsersList";
 
 const Users = () => {
-  const USERS = [
-    {
-      id: "u1",
-      image:
-        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-      name: "Maximilian",
-      places: 3,
-    },
-  ];
   return <UsersList items={USERS} />;
 };
 
