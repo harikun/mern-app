020fba321a333c8fe18ac7ec2576a133f1908bff
Cipto Suhari <cipto.suheri@gmail.com>
2022-03-28 20:50:37 +0700

    getting user with GET request

--- FRONTEND/src/user/pages/Users.jsx
+++ FRONTEND/src/user/pages/Users.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import UsersList from "../components/UsersList";
 
 const Users = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [loadedUsers, setLoadedUsers] = useState();
+  useEffect(() => {
+    const sendRequest = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch("https://localhost:5001/api/users");
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        setLoadedUsers(responseData.users);
+      } catch (err) {
+        setError(err.message);
+      }
+      setIsLoading(false);
+    };
+    sendRequest();
+  }, []);
   return <UsersList items={USERS} />;
 };
 
