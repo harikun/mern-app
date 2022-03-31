726cc865e1324bf7ffac3ffe35cbfe4a5cc2ce1a
Cipto Suhari <cipto.suheri@gmail.com>
2022-03-31 21:08:03 +0700

    Loading places by user Id

--- FRONTEND/src/places/pages/UserPlaces.jsx
+++ FRONTEND/src/places/pages/UserPlaces.jsx
@@ -2,12 +2,38 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import PlaceList from "../components/PlaceList";
 import { useHttpClient } from "../../hooks/http-hook";
+import ErrorModal from "../../components/ErrorModal";
+import LoadingSpinner from "../../components/LoadingSpinner";
+import { useEffect } from "react";
 
 const UserPlaces = () => {
+  const [loadedPlaces, setLoadedPlaces] = React.useState();
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
   const userId = useParams().userId;
-  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
-  return <PlaceList items={loadedPlaces} />;
+
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      try {
+        const responseData = await sendRequest(
+          `${process.env.REACT_APP_BACKEND_URL}/places/user/${userId}`
+        );
+        setLoadedPlaces(responseData.places);
+      } catch (err) {}
+    };
+    fetchPlaces();
+  }, [sendRequest, userId]);
+  return (
+    <>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+    </>
+  );
 };
 
 export default UserPlaces;
