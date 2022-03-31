22384dd39fdb053a8e5b51b9a10307964726423d
Cipto Suhari <cipto.suheri@gmail.com>
2022-03-31 20:57:01 +0700

    import custom hook

--- FRONTEND/src/places/pages/UserPlaces.jsx
+++ FRONTEND/src/places/pages/UserPlaces.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import PlaceList from "../components/PlaceList";
+import { useHttpClient } from "../../hooks/http-hook";
 
 const UserPlaces = () => {
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
   const userId = useParams().userId;
   const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
   return <PlaceList items={loadedPlaces} />;
